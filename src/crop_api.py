10dc4d85bd843e9a1bb4d40ac660e4f15e64237a
Mukilan Krishnakumar <71809485+Mukilan-Krishnakumar@users.noreply.github.com>
2023-11-14 20:58:33 +0530

    Update crop_api.py

--- src/crop_api.py
+++ src/crop_api.py
@@ -251,6 +251,7 @@ class ImageSaliencyModel(object):
         img_h, img_w = img.shape[:2]
 
         print(aspectRatios, img_w, img_h)
+        print("Checking if this change is relfected")
 
         if aspectRatios is None:
             aspectRatios = self.aspectRatios
@@ -258,8 +259,9 @@ class ImageSaliencyModel(object):
         if aspectRatios is None:
             aspectRatios = [0.56, 1.0, 1.14, 2.0, img_h / img_w]
 
+        print("Print before output")
         output = self.get_output(img_path, aspectRatios=aspectRatios)
-        print("Got this output\n", output)
+        print("This is the output", output)
         n_crops = len(output["crops"])
         salient_x, salient_y, = output[
             "salient_point"
